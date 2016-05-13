1182e79171d935107daf9d3bcdfec5e9afb11e93
Christoph Budjan <cbudjan@gmail.com>
2016-05-13 11:20:25 -0400

    Initial configuration

---
+++ home/.aliases.zsh
@@ -0,0 +1,21 @@
+# FileSearch
+function f() { find . -iname "*$1*" ${@:2} }
+function r() { grep "$1" ${@:2} -R . }
+
+#mkdir and cd
+function mkcd() { mkdir -p "$@" && cd "$_"; }
+
+alias c=clear
+
+# Empty the Trash on all mounted volumes and the main HDD. then clear the useless sleepimage
+alias emptytrash=" \
+    sudo rm -rfv /Volumes/*/.Trashes; \
+    rm -rfv ~/.Trash/*; \
+    sudo rm -v /private/var/vm/sleepimage; \
+     \
+"
+
+# Recursively delete `.DS_Store` files
+alias cleanup_dsstore="find . -name '*.DS_Store' -type f -ls -delete"
+
+alias f='open -a Finder ./'
