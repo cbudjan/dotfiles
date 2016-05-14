f3e1ba9eb26aca605f2cd350c6b340cef3868b4a
Christoph Budjan <cbudjan@gmail.com>
2016-05-14 16:56:28 -0400

    added more aliases

--- home/.aliases.zsh
+++ home/.aliases.zsh
@@ -10,12 +10,25 @@ alias c=clear
 # Empty the Trash on all mounted volumes and the main HDD. then clear the useless sleepimage
 alias emptytrash=" \
     sudo rm -rfv /Volumes/*/.Trashes; \
-    rm -rfv ~/.Trash/*; \
+    sudo rm -rfv ~/.Trash; \
     sudo rm -v /private/var/vm/sleepimage; \
-     \
+    sudo rm -rfv /private/var/log/asl/*.asl \
 "
+
+alias update='sudo softwareupdate -i -a; brew update; brew upgrade; brew cleanup; brew prune; sudo npm update npm -g; sudo npm update -g; sudo gem update'
 
 # Recursively delete `.DS_Store` files
 alias cleanup_dsstore="find . -name '*.DS_Store' -type f -ls -delete"
 
 alias f='open -a Finder ./'
+
+alias brewing="brew update && brew upgrade && brew cleanup && brew prune"
+alias cpu='top -o cpu' # cpu
+alias mem='top -o rsize' # memory
+alias ip="curl icanhazip.com"
+
+# alias last command
+a() {
+  x=`history 1 | sed 's/.\{7\}//'`;
+  alias ${1}="${x}";
+}
